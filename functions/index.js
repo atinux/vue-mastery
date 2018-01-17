d18469b67bcbad9de2199244e3c5502c816a6910
Pierre Macbook Air <schweiger.pierre@gmail.com>
2018-01-17 12:31:58 +0100

    Comment stripe code until we get the proper api key

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,14 @@
 const functions = require('firebase-functions')
-const admin = require('firebase-admin')
+// const admin = require('firebase-admin')
 const Mailerlite = require('mailerlite')
-const stripe = require('stripe')(functions.config().stripe.token)
+// const stripe = require('stripe')(functions.config().stripe.token)
 
 // Configure Mailerlite
 const mailerlite = new Mailerlite(functions.config().mailerlite.key)
 const mailerliteSubscribers = mailerlite.Subscribers
 
 // Configure stripe
-stripe.currency = functions.config().stripe.currency || 'USD'
+// stripe.currency = functions.config().stripe.currency || 'USD'
 
 // Sends an email confirmation when a user changes his mailing list subscription.
 exports.sendEmailConfirmation = functions.database.ref('/users/{uid}').onWrite(event => {
@@ -24,20 +24,20 @@ exports.createCustomer = functions.auth.user().onCreate(event => {
   const user = event.data
   const displayName = user.displayName
 
-  mailerliteSubscribers.addSubscriber('8738992', user.email, displayName, {}, 1)
+  return mailerliteSubscribers.addSubscriber('8738992', user.email, displayName, {}, 1)
     .then((res) => {
       console.log(res)
     })
 
   // register Stripe user
-  return stripe.customers.create({
-    email: user.email
-  })
-    .then(customer => {
-      // update database with stripe customer id
-      const updates = {}
-      updates[`/customers/${customer.id}`] = user.uid
-      updates[`/users/${user.uid}/customerId`] = customer.id
-      return admin.database().ref().update(updates)
-    })
+  // return stripe.customers.create({
+  //   email: user.email
+  // })
+  //   .then(customer => {
+  //     // update database with stripe customer id
+  //     const updates = {}
+  //     updates[`/customers/${customer.id}`] = user.uid
+  //     updates[`/users/${user.uid}/customerId`] = customer.id
+  //     return admin.database().ref().update(updates)
+  //   })
 })
