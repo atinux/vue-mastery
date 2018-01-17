140951badf40131226fb2c19fdefbcd09d9260fc
Pierre Macbook Air <schweiger.pierre@gmail.com>
2018-01-17 12:13:29 +0100

    Subscribe new user to mailerlist

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const Mailerlite = require('mailerlite')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Configure Mailerlite
+const mailerlite = new Mailerlite(functions.config().mailerlite.key)
+const mailerliteSubscribers = mailerlite.Subscribers
+
+exports.createCustomer = functions.auth.user().onCreate(event => {
+  // user auth data
+  const user = event.data
+  const displayName = user.displayName
+
+  return mailerliteSubscribers.addSubscriber('8738992', user.email, displayName, {}, 1)
+    .then((res) => {
+      console.log(res)
+    })
+})
