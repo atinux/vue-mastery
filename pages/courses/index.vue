83c0c3c6fa63dabdfaf4d9bde333b4be0dd7dd13
Pierre Macbook Air <schweiger.pierre@gmail.com>
2018-01-23 10:30:04 +0100

    Add courses list to courses page

--- pages/courses/index.vue
+++ pages/courses/index.vue
@@ -1,14 +1,19 @@
-<template>
-  <div>
-    <h1>Courses page</h1>
-  </div>
+<template lang="pug">
+  div
+    h1 Courses page
+    CourseList
 </template>
 
 <script>
+import CourseList from '~/components/courses/List'
+
 export default {
   middleware: 'anonymous',
   head: {
     title: 'Courses page'
+  },
+  components: {
+    CourseList
   }
 }
 </script>
