6526ee0848648c506ac84778e4336d7708915b0f
whosdustin <dustin@delatore.me>
2018-01-26 19:03:20 +0700

    Move Hero to components library

---
+++ components/static/Hero.vue
@@ -0,0 +1,73 @@
+<template lang="pug">
+.banner
+  .body
+    h1.title The ultimate learning resource for Vue developers
+    p.lead Weekly Vue.js tutorials to guide your journey to Mastery.
+  .player
+    .play-button
+  .actions
+    nuxt-link.button.secondary(to="/courses") START LEARNING
+</template>
+
+<script>
+export default {
+  'name': 'Hero'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '../../assets/css/_variables'
+  .banner
+    grid-template-columns: auto
+    grid-template-rows: ($vertical-space/1.5) auto auto auto ($vertical-space/1.5)
+    align-items: center
+    justify-content: center
+    background-image url(/images/hero.svg)
+    background-position: 85% top
+    background-size: 185%
+    background-repeat: no-repeat
+  .body
+    display: grid
+    grid-row-start: 2
+    justify-content: center
+    align-items: center
+    text-align: center
+    .title
+      max-width: 800px
+      margin: 0
+      color: $secondary-color
+    .lead
+      justify-self: center
+      max-width: 690px
+      font-size: 24px
+      color: #555555
+  .player
+    grid-row-start: 3
+    height 379px
+    max-width: 100%
+    background-color $black
+    display flex
+    align-items center
+    justify-content center
+  .play-button
+    height 70px
+    width 70px
+    background-color #FFFFFF
+    border-radius 50%
+    display flex
+    align-items center
+    justify-content center
+    &:after
+      content ''
+      width 0px
+      height 0px
+      border-top 12px solid transparent
+      border-bottom 12px solid transparent
+      border-left 20px solid $black
+      margin-right -7px
+  .actions
+    display: grid
+    grid-row-start: 4
+    justify-content: center
+    align-items: center
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,7 @@
 <template lang="pug">
   div
-    .hero
-      .container
-        .hero-body
-          h1.title The ultimate learning resource for Vue developers
-          p Weekly Vue.js tutorials to guide your journey to Mastery.
-          nuxt-link.button(to="/courses") START LEARNING
-
-        .hero-placeholder
-          .hero-play
+    .container
+      Hero
 
     .container
       CourseList
@@ -25,6 +18,7 @@
 </template>
 
 <script>
+import Hero from '~/components/static/Hero'
 import CourseList from '~/components/courses/List'
 import CommunitySupport from '~/components/static/CommunitySupport'
 import CheatSheetMain from '~/components/static/CheatSheetMain'
@@ -36,6 +30,7 @@ export default {
     title: 'Home page'
   },
   components: {
+    Hero,
     CourseList,
     CommunitySupport,
     CheatSheetMain,
@@ -45,71 +40,6 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-// TODO: refactor basic css
 
-.title
-  height 120px
-  width 648px
-  color #36495E
-  width 100%
-  max-width 648px
-
-.hero
-  background-image url(/images/hero.svg)
-  background-size cover
-
-  .container
-    height 590px
-    display flex
-    flex-wrap wrap
-    align-items center
-    justify-content space-between
-
-$black = #222
-.hero-body
-  padding 40px 20px
-  p
-    color $black
-    font-size 20px
-    line-height 28px
-    padding-top: 20px
-
-.hero-placeholder
-  height 379px
-  width 647px
-  max-width: 100%
-  background-color $black
-  display flex
-  align-items center
-  justify-content center
-
-.hero-play
-  height 70px
-  width 70px
-  background-color #FFFFFF
-  border-radius 50%
-  display flex
-  align-items center
-  justify-content center
-
-  &:after
-    content ''
-    width 0px
-    height 0px
-    border-top 12px solid transparent
-    border-bottom 12px solid transparent
-    border-left 20px solid $black
-    margin-right -7px
-
-.button
-  display flex
-  align-items center
-  justify-content center
-  color: #ffffff
-  height 53px
-  width 223px
-  border-radius 26.5px
-  margin-top: 20px
-  background-color #36495E
 
 </style>
