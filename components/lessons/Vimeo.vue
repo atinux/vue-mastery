7bb1cb98f94e730ea88fa567eabd0528098e992d
Pierre Macbook Air <schweiger.pierre@gmail.com>
2018-01-30 21:43:20 +0100

    Add responsive video embed

--- components/lessons/Vimeo.vue
+++ components/lessons/Vimeo.vue
@@ -14,6 +14,21 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
+<style lang="stylus">
+.lesson-video
+  position: relative
+  padding-bottom: 56.25%
+  height: 0
+  overflow: hidden
+  max-width: 100%
 
+  > div,
+  iframe,
+  object,
+  embed
+    position: absolute !important
+    top: 0 !important
+    left: 0 !important
+    width: 100% !important
+    height: 100% !important
 </style>
