2f844037aa29d466bf677aee324a539beee39108
Pierre Macbook Air <schweiger.pierre@gmail.com>
2018-02-07 16:55:34 +0100

    Add local test file

---
+++ functions/test.js
@@ -0,0 +1,17 @@
+subscribeUserToMailerliteCourse(
+  {
+    before: {
+      started: true,
+      subscribed: false
+    },
+    after: {
+      started: true,
+      subscribed: true
+    }
+  }, {
+    params: {
+      cid: '1516789793545',
+      uid: 'KWuKWA7WpledvvvwrCDL6lFYKbT2'
+    }
+  }
+)
