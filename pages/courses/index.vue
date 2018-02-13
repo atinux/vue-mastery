91ccc80c5f2d6255751c87217fcce6167a5382a6
whosdustin <dustin@delatore.me>
2018-02-13 11:52:41 +0700

    Fix Course List header on large screens
    Addresses:
    - The header gets cut off on the right in larger browser sizes. Try
    dragging the browser width around.

--- pages/courses/index.vue
+++ pages/courses/index.vue
@@ -35,29 +35,30 @@ export default {
 
 .container
   > div
-    padding-left: 4%
-    padding-right: 4%
+    padding-left 4%
+    padding-right 4%
 
 .courses-header
-  height: 220px
-  display: flex
-  align-items: center
-  background: url(/static/images/bkg-courses.svg) no-repeat
+  height 220px
+  display flex
+  align-items center
+  background url(/static/images/bkg-courses.svg) no-repeat
+  background-size cover
 
 .title
-  margin: 0
-  font-size: 60px
+  margin 0
+  font-size 60px
   font-weight 600
-  color: #FFFFFF
-  line-height: 60px
+  color #FFFFFF
+  line-height 60px
 
 .courses-body
-  display: grid
-  width: 100%
-  grid-column-gap: 2%
-  grid-row-gap: 45px
-  padding: 60px 4%
+  display grid
+  width 100%
+  grid-column-gap 2%
+  grid-row-gap 45px
+  padding 60px 4%
 
   +laptop-up()
-    grid-template-columns: 63% 35%
+    grid-template-columns 63% 35%
 </style>
