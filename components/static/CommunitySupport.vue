a1050138f0bf3d8492710b592ac698fb32d0e9b7
whosdustin <dustin@delatore.me>
2018-02-14 15:52:55 +0700

    Remove the hand and $5 in community support
    Also, added 4k wrapper

--- components/static/CommunitySupport.vue
+++ components/static/CommunitySupport.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   .banner
-    .body
-      h2.h1.title We Support the Community
-      p.lead In addition to supporting your journey to Vue mastery, we also support Vue.js itself. $5 of your monthly subscription goes directly to the Vue.js organization, supporting its sustainability and growth.
-      span.h1.amount $5
+    .wrapper
+      .body
+        h2.h1.title We Support the Community
+        p.lead In addition to supporting your journey to Vue mastery, we also support Vue.js itself. $5 of your monthly subscription goes directly to the Vue.js organization, supporting its sustainability and growth.
 </template>
 
 <script>
@@ -12,13 +12,16 @@ export default { name: 'CommunitySupport' }
 
 <style lang="stylus" scoped>
   @import '~assets/css/_variables'
-  .banner
+  .wrapper
     grid-template-rows $vertical-space 1fr $vertical-space
     align-items center
     justify-content center
     text-align center
+  .banner
     background-image url(/images/bkg-giveback.svg)
-
+    +desktop-up()
+      background-size contain
+      background-repeat repeat-x
   .body
     grid-row-start 2
 
@@ -34,7 +37,7 @@ export default { name: 'CommunitySupport' }
   .amount
     color $primary-color
     margin 0
-    
+
   +tablet-up()
     .lead
       font-size 24px
