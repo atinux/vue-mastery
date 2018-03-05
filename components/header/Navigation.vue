3c15a55dd4bc868d773bcf1b0592dd5c63346601
whosdustin <dustin@delatore.me>
2018-03-05 08:52:55 +0700

    move dashboard link in navigation:

--- components/header/Navigation.vue
+++ components/header/Navigation.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
   nav.navbar(role="navigation" aria-label="main navigation" @click="closeNav()")
     .navbar-main
+      router-link.navbar-item(to="/account" v-if="account") Dashboard
       router-link.navbar-item(to="/courses") Courses
       router-link.navbar-item(to="/about") About
       router-link.navbar-item(to="/contact") Contact
-      router-link.navbar-item(to="/account" v-if="account") Dashboard
     no-ssr
       .navbar-secondary(v-cloak v-if="account")
         button.button.primary.-small(type="button" v-on:click="signOut") Sign Out
