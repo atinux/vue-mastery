1201a1424dfcf291dfac82382b8454102be2318f
Pierre Macbook Air <schweiger.pierre@gmail.com>
2018-03-06 18:18:02 +0700

    Fix new user subscription to mailer list + send email from contact form change

--- functions/subscription.js
+++ functions/subscription.js
@@ -3,14 +3,21 @@ const Mailerlite = require('mailerlite')
 const mailerlite = new Mailerlite(functions.config().mailerlite.key)
 const mailerliteSubscribers = mailerlite.Subscribers
 const mailerliteList = mailerlite.Lists
-const SparkPost = require('sparkpost')
+// const SparkPost = require('sparkpost')
 
-// Configure Mailerlite
-const mainListId = functions.config().mailerlite.mainListId // "VueMastery.com New Users" group
+const nodemailer = require('nodemailer')
+const gmailEmail = functions.config().gmail.email
+const gmailPassword = functions.config().gmail.password
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword
+  }
+})
 
 module.exports = {
   subscribeUser (account, listID, isSubcribing) {
-    if (listID === null) listID = mainListId
     if (isSubcribing) {
       console.log(`Subscribe ${account.email} to mailing list ${listID}`)
       return mailerliteSubscribers.addSubscriber(listID, account.email, account.displayName, {}, 1)
@@ -34,16 +41,28 @@ module.exports = {
   },
 
   sendContactEmail (val) {
-    const client = new SparkPost(functions.config().sparkpost.key)
-    client.transmissions.send({
-      transmissionBody: {
-        recipients: [{ address: { email: 'schweiger.pierre@gmail.com' } }],
-        content: { template_id: 'contact' },
-        substitution_data: {'email': val.email, 'name': val.name, 'message': val.message},
-        campaign_id: 'Contact'
-      }
-    }, (err, res) => {
-      console.warn(err ? err.stack : `Email Sent for test to ${val.email}`)
+    // const client = new SparkPost(functions.config().sparkpost.key)
+    // client.transmissions.send({
+    //   transmissionBody: {
+    //     recipients: [{ address: { email: 'schweiger.pierre@gmail.com', name: 'Vue Mastery Team' } }],
+    //     content: { template_id: 'contact' },
+    //     substitution_data: {'email': val.email, 'name': val.name, 'message': val.message},
+    //     campaign_id: 'Contact'
+    //   }
+    // }, (err, res) => {
+    //   console.warn(err ? err.stack : `Email Sent for test to ${val.email}`)
+    // })
+    const mailOptions = {
+      from: 'Vue Mastery <noreply@firebase.com>',
+      // to: 'team@vuemastery.com'
+      to: 'schweiger.pierre@gmail.com'
+    }
+
+    // The user subscribed to the newsletter.
+    mailOptions.subject = `Vue Mastery website Request`
+    mailOptions.text = `Message from ${val.name}: ${val.message}`
+    return mailTransport.sendMail(mailOptions).then(() => {
+      return console.log('New email sent from:', val.name)
     })
   }
 }
